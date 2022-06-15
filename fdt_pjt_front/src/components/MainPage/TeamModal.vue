ac2b70079a4b7a131e64ac4acbdc7bc6008279e2
PARK SIWON <zow777@naver.com>
2022-06-16 00:10:53 +0900

    220616 SW: mainpage update

---
+++ fdt_pjt_front/src/components/MainPage/TeamModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel"></h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TeamModal',
+
+}
+</script>
+
+<style>
+
+</style>
